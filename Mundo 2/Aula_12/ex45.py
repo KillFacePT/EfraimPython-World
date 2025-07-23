4ae989ef949f339d48ae61888e3e63a4c3e0205c
KillFacePT <21142629+KillFacePT@users.noreply.github.com>
2025-07-23 02:24:05 +0100

    Update ex45.py

--- Mundo 2/Aula_12/ex45.py
+++ Mundo 2/Aula_12/ex45.py
@@ -1,57 +1,68 @@
-
-# Crie um programa que faca o computador jogar JOkenpô com voce
 import random
-from time import sleep
+from time import sleep as slp
 
-jokenpo = ['Pedra', 'Papel', 'Tesoura']
+print('\033[1;31m-'*16,'\033[4;33mJokenpô\033[m','\033[1;31m-\033[m'*17)
 
-computador = random.choice(jokenpo)
+ppt = ['pedra','papel','tesoura']
+rppt = random.choice(ppt)
 
-print('''Suas opções: 
-[0] PEDRA 
-[1] PAPEL 
-[2] TESOURA''')
+esc = str(input('\033[0;33mEscreve pedra, papel ou tesoura: ')).strip().lower()
+print('\033[1;31m-'*42)
+slp(1)
+if esc not in ['pedra', 'papel', 'tesoura']:
+   print('\033[1;31mPara jogares deves escrever pedra, papel ou tesoura!')
+else:
+    print('\033[1;4;31;43m','Vamos Jogar'.center(40),'\033[m\033[1;31m')
+    slp(3)
+    print(f'Pedra'.center(42),'\033[m\033[1;31m')
+    slp(1)
+    print(f'Papel'.center(42),'\033[m\033[1;31m')
+    slp(1)
+    print(f'Tesoura'.center(42),'\033[m')
+    slp(1)
+    print('\033[1;31m-'*42)
 
-jogador = int(input("Qual é a sua jogada? "))
 
-print("JO")
-sleep(1)
-print("KEN")
-sleep(1)
-print("PO")
-print('-=' * 11)
-print(f"Computador jogou {computador}")
-print(f"Jogador jogou {jokenpo[jogador]}")
-print('-=' * 11)
+    if rppt == 'pedra':
+        print('\033[1;33mEu escolhi\033[1;31m Pedra')
+        if esc == 'pedra':
+            print('\033[1;33mEu escolhi\033[1;31m Pedra\033[1;33m. Empatamos!')
+
+        elif esc == 'tesoura':
+            print(f'\033[1;33mEu escolhi\033[1;34m Pedra\033[1;33m.\033[1;31m PERDESTE! \033[1;33mA\033[1;34m Pedra\033[1;33m ganha da \033[1;34m Tesoura\033[1;33m!')
+
+        elif esc == 'papel':
+            print(f'\033[1;33mEu escolhi\033[1;34m Pedra\033[1;33m.\033[1;32m GANHASTE! \033[1;33mO\033[1;34m Papel\033[1;33m ganha da \033[1;34m Pedra\033[1;33m!')
+
+        else:
+            print()
 
 
-if computador == jokenpo[0]:  # computador jogou pedra
-    if jogador == 0:
-        print("Empate")
-    
-    elif jogador == 1:
-        print("Jogador Vence")
+    elif rppt == 'papel':
+        print('\033[1;33mEu escolhi\033[1;31m Papel')
+        if esc == 'papel':
+            print('\033[1;33mEu escolhi\033[1;31m Papel\033[1;33m. Empatamos!')
 
-    elif jogador == 2:
-        print("Computador Vence")
+        elif esc == 'pedra':
+            print(f'\033[1;33mEu escolhi\033[1;34m Papel\033[1;33m.\033[1;31m PERDESTE! \033[1;33mO\033[1;34m Papel\033[1;33m ganha da \033[1;34m Pedra\033[1;33m!')
 
-elif computador == jokenpo[1]: # computador jogou papel
-    if jogador == 0:
-        print("Computador Vence")
+        elif esc== 'tesoura':
+            print(f'\033[1;33mEu escolhi\033[1;34m Papel\033[1;33m.\033[1;32m GANHASTE! \033[1;33mA\033[1;34m Tesoura\033[1;33m ganha do \033[1;34m Papel\033[1;33m!')
 
-    elif jogador == 1:
-        print("Empate")
+        else:
+            print()
 
-    elif jogador == 2:
-        print("Jogador Vence")
+    elif rppt == 'tesoura':
+        print('\033[1;33mEu escolhi\033[1;31m Tesoura')
+        if esc == 'tesoura':
+            print('\033[1;33mEu escolhi\033[1;31m Tesoura\033[1;33m. Empatamos!')
 
-elif computador == jokenpo[2]: # computador jogou tesoura
-    if jogador == 0:
-        print("Jogador Venceu")
+        elif esc == 'papel':
+            print(f'\033[1;33mEu escolhi\033[1;34m Pedra\033[1;33m.\033[1;31m PERDESTE! \033[1;33mA\033[1;34m Tesoura\033[1;33m ganha do \033[1;34m Papel\033[1;33m!')
 
-    elif jogador == 1:
-        print("Computador Vence")
-
-    elif jogador == 2:
-        print("Empate")
-
+        elif esc == 'pedra':
+            print(f'\033[1;33mEu escolhi\033[1;34m Pedra\033[1;33m.\033[1;32m GANHASTE! \033[1;33mA\033[1;34m Pedra\033[1;33m ganha da \033[1;34m Tesoura\033[1;33m!')
+        else:
+            print()
+    else:
+        print()
