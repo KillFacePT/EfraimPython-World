88b99a97686bd10a57ea9a8aa4d4f259d732f50b
KillFacePT <21142629+KillFacePT@users.noreply.github.com>
2025-07-23 02:19:12 +0100

    Update ex39.py

--- Mundo 2/Aula_12/ex39.py
+++ Mundo 2/Aula_12/ex39.py
@@ -1,32 +1,23 @@
-
-# faça um programa que leia o ano de nascimento de um jovem e informe, de com acordo com sua idade 
-# se ele ainda vai se alistar ao serviço militar
-# se é a hora de se alistar
-# se ja passou do tempo do alistamento 
-# o programa tambem deverá mostrar o tempo que falta ou que passou do prazo 
-
-
 from datetime import date
+curyear = date.today().year
 
-ano_nasc = int(input("Qual ano você nasceu? "))
+print('\033[1;31m-'*10,'\033[4;33mCalculadora de Alistamento\033[m','\033[1;31m-\033[m'*10)
+birth = int(input('\033[0;33mEscreva o seu ano de nascimento: '))
+idd = curyear - birth
 
-ano_atual = date.today().year
+print('\033[1;31m-'*48)
+if idd >= 18:
+    print('\033[1;31m1\033[m \033[33m= \033[32mSim'.center(60))
+    print('\033[1;31m2\033[m \033[33m= \033[32mNão'.center(60))
+    ynlist = int(input('\033[33mSe já se alistou escreva \033[1;31m1\033[0;33m caso contrario escreva \033[1;31m2\033[33m: '))
+    print('\033[1;31m-'*48)
 
-idade = ano_atual - ano_nasc
-
-print(" ")
-print(f"Nascimento: {ano_nasc}")
-print(f"Sua idade: {idade}")
-print(" ")
-
-if idade > 18:
-    prazo = idade - 18
-    print(f"Você ja passou do prazo de {prazo} ano(s) para o alistamento.")
-
-elif idade < 18:
-    prazo = 18 - idade
-    alista = ano_atual + prazo
-    print(f"Voce irá se alistar em {prazo} ano(s) em {alista}")
-
-elif idade == 18:
-    print(f"Você está com {idade} anos e está na hora de se alistar.")
+    if ynlist == 1:
+        print('\033[32mObrigado \033[33mpor ter cumprido o seu dever militar!')
+    elif idd > 18:
+        print(f'\033[1;4;31mAtenção!!!\033[0;33m Já deveria ter feito o seu alistamento há \033[1;31m{idd - 18}\033[0;33m {'ano' if idd - 18 < 2 else 'anos'}!')
+    else:
+        print(f'\033[1;4;31mAtenção!!!\033[0;33m Está na altura de fazer o seu alistamento!')
+else:
+    print(
+        f'\033[1;4;31mAtenção!!!\033[0;33m Faltam \033[1;31m{18 - idd}\033[0;33m {'ano' if idd - 18 < 2 else 'anos'} o seu alistamento!')
